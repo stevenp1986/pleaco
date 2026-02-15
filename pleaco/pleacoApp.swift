8684133f53c98e21977c44d6045a42d040800f78
letzgo <daniel@letzgo.me>
2026-02-15 10:18:14 +0100

    Initial Commit

---
+++ pleaco/pleacoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pleacoApp.swift
+//  pleaco
+//
+//  Created by Daniel Goletz on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct pleacoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
